d432c726ea16066869eb04a217f570a9fd67c5c7
Jeroen Ooms <jeroenooms@gmail.com>
2019-03-15 12:12:28 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,85 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# arrow
+
+R integration with Apache Arrow.
+
+## Installation
+
+You first need to install the C++ library:
+
+### macOS
+
+On macOS, you may install using homebrew:
+
+    brew install apache-arrow
+
+### From source
+
+First install a release build of the C++ bindings to arrow.
+
+``` shell
+git clone https://github.com/apache/arrow.git
+cd arrow/cpp && mkdir release && cd release
+
+# It is important to statically link to boost libraries
+cmake .. -DARROW_PARQUET=ON -DCMAKE_BUILD_TYPE=Release -DARROW_BOOST_USE_SHARED:BOOL=Off
+make install
+```
+
+## Then the R package
+
+``` r
+devtools::install_github("apache/arrow/r")
+```
+
+## Example
+
+``` r
+library(arrow)
+#> 
+#> Attaching package: 'arrow'
+#> The following object is masked from 'package:utils':
+#> 
+#>     timestamp
+#> The following objects are masked from 'package:base':
+#> 
+#>     array, table
+
+(tib <- tibble::tibble(x = 1:10, y = rnorm(10)))
+#> # A tibble: 10 x 2
+#>        x       y
+#>    <int>   <dbl>
+#>  1     1  0.585 
+#>  2     2  0.378 
+#>  3     3  0.958 
+#>  4     4 -0.321 
+#>  5     5  0.702 
+#>  6     6  0.188 
+#>  7     7 -0.625 
+#>  8     8 -2.34  
+#>  9     9 -0.0325
+#> 10    10 -1.22
+tab <- table(tib)
+tab$schema
+#> arrow::Schema 
+#> x: int32
+#> y: double
+tab
+#> arrow::Table
+as_tibble(tab)
+#> # A tibble: 10 x 2
+#>        x       y
+#>    <int>   <dbl>
+#>  1     1  0.585 
+#>  2     2  0.378 
+#>  3     3  0.958 
+#>  4     4 -0.321 
+#>  5     5  0.702 
+#>  6     6  0.188 
+#>  7     7 -0.625 
+#>  8     8 -2.34  
+#>  9     9 -0.0325
+#> 10    10 -1.22
+```
---
+++ src/array.cpp
@@ -0,0 +1,118 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "./arrow_types.h"
+
+using Rcpp::LogicalVector;
+using Rcpp::no_init;
+
+// [[Rcpp::export]]
+std::shared_ptr<arrow::Array> Array__Slice1(const std::shared_ptr<arrow::Array>& array,
+                                            int offset) {
+  return array->Slice(offset);
+}
+
+// [[Rcpp::export]]
+std::shared_ptr<arrow::Array> Array__Slice2(const std::shared_ptr<arrow::Array>& array,
+                                            int offset, int length) {
+  return array->Slice(offset, length);
+}
+
+// [[Rcpp::export]]
+bool Array__IsNull(const std::shared_ptr<arrow::Array>& x, int i) { return x->IsNull(i); }
+
+// [[Rcpp::export]]
+bool Array__IsValid(const std::shared_ptr<arrow::Array>& x, int i) {
+  return x->IsValid(i);
+}
+
+// [[Rcpp::export]]
+int Array__length(const std::shared_ptr<arrow::Array>& x) { return x->length(); }
+
+// [[Rcpp::export]]
+int Array__offset(const std::shared_ptr<arrow::Array>& x) { return x->offset(); }
+
+// [[Rcpp::export]]
+int Array__null_count(const std::shared_ptr<arrow::Array>& x) { return x->null_count(); }
+
+// [[Rcpp::export]]
+std::shared_ptr<arrow::DataType> Array__type(const std::shared_ptr<arrow::Array>& x) {
+  return x->type();
+}
+
+// [[Rcpp::export]]
+std::string Array__ToString(const std::shared_ptr<arrow::Array>& x) {
+  return x->ToString();
+}
+
+// [[Rcpp::export]]
+arrow::Type::type Array__type_id(const std::shared_ptr<arrow::Array>& x) {
+  return x->type_id();
+}
+
+// [[Rcpp::export]]
+bool Array__Equals(const std::shared_ptr<arrow::Array>& lhs,
+                   const std::shared_ptr<arrow::Array>& rhs) {
+  return lhs->Equals(rhs);
+}
+
+// [[Rcpp::export]]
+bool Array__ApproxEquals(const std::shared_ptr<arrow::Array>& lhs,
+                         const std::shared_ptr<arrow::Array>& rhs) {
+  return lhs->ApproxEquals(rhs);
+}
+
+// [[Rcpp::export]]
+std::shared_ptr<arrow::ArrayData> Array__data(
+    const std::shared_ptr<arrow::Array>& array) {
+  return array->data();
+}
+
+// [[Rcpp::export]]
+bool Array__RangeEquals(const std::shared_ptr<arrow::Array>& self,
+                        const std::shared_ptr<arrow::Array>& other, int start_idx,
+                        int end_idx, int other_start_idx) {
+  return self->RangeEquals(*other, start_idx, end_idx, other_start_idx);
+}
+
+// [[Rcpp::export]]
+LogicalVector Array__Mask(const std::shared_ptr<arrow::Array>& array) {
+  if (array->null_count() == 0) {
+    return LogicalVector(array->length(), true);
+  }
+
+  auto n = array->length();
+  LogicalVector res(no_init(n));
+  arrow::internal::BitmapReader bitmap_reader(array->null_bitmap()->data(),
+                                              array->offset(), n);
+  for (size_t i = 0; i < array->length(); i++, bitmap_reader.Next()) {
+    res[i] = bitmap_reader.IsSet();
+  }
+  return res;
+}
+
+// [[Rcpp::export]]
+std::shared_ptr<arrow::Array> DictionaryArray__indices(
+    const std::shared_ptr<arrow::DictionaryArray>& array) {
+  return array->indices();
+}
+
+// [[Rcpp::export]]
+std::shared_ptr<arrow::Array> DictionaryArray__dictionary(
+    const std::shared_ptr<arrow::DictionaryArray>& array) {
+  return array->dictionary();
+}
